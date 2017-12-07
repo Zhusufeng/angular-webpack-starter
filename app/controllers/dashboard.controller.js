346601b7ac471c45bd5e54fe7d624e58367102ad
Lisa Gee <zhusufeng@gmail.com>
2017-12-07 09:09:07 -0800

    Take out success and replace with then

--- app/controllers/dashboard.controller.js
+++ app/controllers/dashboard.controller.js
@@ -4,7 +4,7 @@ DashboardController.$inject = ['GithubStatusService'];
 function DashboardController(gh) {
     var _this = this;
     _this.github = '';
-    gh.getStatus().success(function(status) {
+    gh.getStatus().then(function(status) {
         _this.github = status;
     });
 }
