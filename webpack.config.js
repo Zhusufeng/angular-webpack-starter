71f693ec99a188f2480c689cf199e8b6717199b9
Lisa Gee <zhusufeng@gmail.com>
2017-12-06 19:34:53 -0800

    Update CommonsChunkPlugin to new syntax

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,9 @@ module.exports = {
     filename: 'app.bundle.js'
   },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.bundle.js")
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor', 
+      filename: 'vendor.bundle.js'
+    })
   ]
 };
